3ca1be04ed4e2a91fcaa58dffe1e3fbad73ee986
Nicolai Bak <79926921+Nicolai-Bak@users.noreply.github.com>
2023-02-17 10:29:27 +0100

    setup/add frontend project (#2)
    * frontend added w/ prittier, eslint and native support
    
    * docker support added
    
    * using internal port instead of localhost
    
    * ocelot gateway added
    
    * about to setup api gateway
    
    * Added new frontend

--- backend/weather/src/App/Program.cs
+++ backend/weather/src/App/Program.cs
@@ -1,8 +1,9 @@
 // CA1852 Type 'Program' can be sealed because it has no subtypes in its containing assembly and is not externally visible
 #pragma warning disable CA1852
 using System.Security.Cryptography;
-
 using App;
+
+const string ALLOW_ALL_ORIGINS = "allow_all_origins";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(
+    options =>
+        options.AddPolicy(
+            name: ALLOW_ALL_ORIGINS,
+            policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+        )
+);
 
 var app = builder.Build();
 
@@ -19,8 +28,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 var summaries = new[]
 {
@@ -53,5 +60,7 @@ app.MapGet(
     )
     .WithName("GetWeatherForecast")
     .WithOpenApi();
+
+app.UseCors(ALLOW_ALL_ORIGINS);
 
 app.Run();
---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
