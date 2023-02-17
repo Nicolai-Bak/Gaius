24cb223b56a786a196a14597e955405b60aa8faf
Nicolai-Bak <nicolai.bak@gmail.com>
2023-02-17 10:34:46 +0100

    Remove dotnet backend

--- backend/weather/src/App/WeatherForecast.cs
+++
@@ -1,7 +0,0 @@
-namespace App;
-
-internal sealed record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    private readonly int num = 3;
-    public int TemperatureF => 32 + (int)(TemperatureC + 0.5556 + num);
-}
